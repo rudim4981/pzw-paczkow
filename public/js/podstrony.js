1fde800931686ba2b3d2ce18857fe7a7311cb097
rudim4981 <48216866+rudim4981@users.noreply.github.com>
2020-01-28 11:38:13 +0100

    Create podstrony.js

---
+++ public/js/podstrony.js
@@ -0,0 +1,17 @@
+		var scrollPos = 0;
+    $( window ).ready(function(){scr()});
+		window.onscroll =  function() {scr()};
+		window.onload = function() {scr()};
+		function menu() {
+			if((document.body.getBoundingClientRect().top > -125) && (scrollPos <= 0)){
+				document.getElementById('info').className = 'def';
+				document.getElementById('info').style.top=document.body.getBoundingClientRect().top;
+			}else{
+				document.getElementById('info').className = 'top';
+			}
+			scrollPos = (document.body.getBoundingClientRect()).top;
+		};
+		function scr(){
+			menu();
+			document.getElementById('progress').style.width = (((innerHeight-document.body.getBoundingClientRect().top-innerHeight))/(document.body.getBoundingClientRect().height - innerHeight))*101 + '%';
+		}
